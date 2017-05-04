26a0176f3fd0e8bf57b61ea00eedbc7d19128fc3
Paulo Henrique Vasconcellos <phsamus@gmail.com>
2017-05-04 10:59:53 -0300

    Renomeio do arquivo dados_consulta.conf e alteração da função solicita_servico

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import win32con, win32api
 import time
+import random
 
 def autentica_usuario():
     """
@@ -9,35 +10,6 @@ def autentica_usuario():
     usuario = input('Digite o nome de usuario: ')
     senha = input('Digite a senha: ')
     return usuario, senha
-
-def solicita_servico():
-    while True:
-        #Solicita ao usuário um tipo de serviço
-        try:            
-            mensagem = '\nQue tipo de serviço quer solicitar? (Digite o número)\n 1 - Consulta;\n 2 - Exame;\n 3 - Internação\n\n Opção: '
-            tipo_de_servico = int(input(mensagem))
-            
-            #Verifica se o status é uma opção válida
-            if tipo_de_servico > 3 or tipo_de_servico < 1:
-                print('Opção inválida. Tente de novo.\n')
-                time.sleep(3)
-            else:
-                break
-        #Em caso de erro, informa ao usuário e tenta de novo
-        except ValueError as erro:
-            print('Opção inválida. Tente de novo.\n')
-            time.sleep(3)
-
-    if tipo_de_servico == 1:
-        url = 'http://ntiss.neki-it.com.br/ntiss//tiss/solicitacaoprocedimento/solicitacaoConsulta/solicitacaoConsulta.jsf'
-        servico = 'Consulta'
-    elif tipo_de_servico == 2:
-        url = 'http://ntiss.neki-it.com.br/ntiss//tiss/solicitacaoprocedimento/solicitacaoSadt/solicitacaoSadtFat.jsf'
-        servico = 'SADT'
-    elif tipo_de_servico == 3:
-        url = 'http://ntiss.neki-it.com.br/ntiss//tiss/solicitacaoprocedimento/solicitacaoInternacao/solicitacaoInternacao.jsf'
-        servico = 'Internacao'
-    return url, servico
 
 def status_servico():
     while True:
@@ -64,9 +36,55 @@ def status_servico():
     elif status == 3:
         return 'Negado'
 
+def solicita_servico():
+    while True:
+        #Solicita ao usuário um tipo de serviço
+        try:            
+            mensagem = '\nQue tipo de serviço quer solicitar? (Digite o número)\n 1 - Consulta;\n 2 - Exame;\n 3 - Internação\n\n Opção: '
+            tipo_de_servico = int(input(mensagem))
+            
+            #Verifica se o status é uma opção válida
+            if tipo_de_servico > 3 or tipo_de_servico < 1:
+                print('Opção inválida. Tente de novo.\n')
+                time.sleep(3)
+            else:
+                break
+        #Em caso de erro, informa ao usuário e tenta de novo
+        except ValueError as erro:
+            print('Opção inválida. Tente de novo.\n')
+            time.sleep(3)
+    #Verifica o status do serviço de acordo com função status_servico
+    status_do_servico = status_servico()
+    
+    #Realizando solicitação
+    if tipo_de_servico == 1:
+        #URL na página da Neki
+        url = 'http://ntiss.neki-it.com.br/ntiss//tiss/solicitacaoprocedimento/solicitacaoConsulta/solicitacaoConsulta.jsf'        
+        driver.get(url)
+
+        #Cada campo da guia de solicitação será armazenada em um dicionário
+        dados_da_guia = dict()
+        #Informando dados do serviço de acordo com o arquivo
+        with open('dados_Consulta_{}.conf'.format(status_do_servico)) as arquivo:
+            for linha in arquivo:
+                (key, value) = linha.split()
+                dados_da_guia[key] = value                
+        print(dados_da_guia)
+        #Informando dados na tela do ntiss
+        driver.find_element_by_id('solicitacaoConsultaForm:numeroGuiaOperadora').send_keys(random.randint(1000))
+        driver.find_element_by_id('solicitacaoConsultaForm:carteira').send_keys(dados_da_guia[carteira])
+        driver.find_element_by_id('solicitacaoConsultaForm:nomeBeneficiario').send_keys(dados_da_guia[nomeBeneficiario])
+
+    elif tipo_de_servico == 2:
+        #URL na página da Neki
+        url = 'http://ntiss.neki-it.com.br/ntiss//tiss/solicitacaoprocedimento/solicitacaoSadt/solicitacaoSadtFat.jsf'
+        servico = 'SADT'
+    elif tipo_de_servico == 3:
+        #URL na página da Neki
+        url = 'http://ntiss.neki-it.com.br/ntiss//tiss/solicitacaoprocedimento/solicitacaoInternacao/solicitacaoInternacao.jsf'
+        servico = 'Internacao'
+
 credenciais = autentica_usuario()
-url_servico = solicita_servico()
-status_do_servico = status_servico()
 
 #Iniciando browser e abrindo página do ntiss
 driver = webdriver.Chrome('C:/Temp/chromedriver.exe')
@@ -78,5 +96,5 @@ driver.find_element_by_id('login').send_keys(credenciais[0])
 driver.find_element_by_id('senha').send_keys(credenciais[1])
 driver.find_element_by_id('botaoEntrar').click()
 
-#Acessando página da solicitação de acordo com função solicita_servico
-driver.get(url_servico[0])
+#Realiza o pedido
+solicita_servico()
